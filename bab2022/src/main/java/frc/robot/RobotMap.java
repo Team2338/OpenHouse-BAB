f7f86948b489da265c5adceac4275136855b5df7
Ahmed F <17001900@students.sd308.org>
2022-08-26 23:10:29 -0500

    bab

---
+++ bab2022/src/main/java/frc/robot/RobotMap.java
@@ -0,0 +1,21 @@
+package frc.robot;
+
+public abstract class RobotMap {
+
+    // ** CAN IDs **
+    // Motors
+    public static final int MOTOR_TALON_ONE = 4;
+    public static final int MOTOR_SPARKMAX_ONE = 23;
+
+    //pigeon when connected in CAN path
+    public static final int PIGEON = 99;
+
+    // ** Joysticks **
+    public static final int DRIVER_CONTROLLER_ID = 0;
+    public static final int AUX_CONTROLLER_ID = 1;
+    public static final int TEST_CONTROLLER_ID = 2;
+
+    // ** DIO ports **
+    public static final int LIMIT_ID = 0;
+
+}
---
+++ bab2022/src/main/java/frc/robot/subsystems/CIM.java
@@ -0,0 +1,34 @@
+package frc.robot.subsystems;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import com.ctre.phoenix.motorcontrol.ControlMode;
+import com.ctre.phoenix.motorcontrol.NeutralMode;
+import com.ctre.phoenix.motorcontrol.can.TalonSRX;
+import frc.robot.RobotMap;
+
+/**
+ * Describes the functionality of the CIM motor
+ * Calling it the CIMShooter
+ */
+public class CIM extends SubsystemBase {
+  private static CIM instance = null;
+
+  // creates a singleton class (only 1 instance can be instantiated)
+  public static CIM getInstance() {
+    if (instance == null) {
+      instance = new CIM();
+    }
+    return instance;
+  }
+
+  private static final TalonSRX CIMShooterMotor   = new TalonSRX(RobotMap.MOTOR_TALON_ONE);
+
+  public CIM() {
+    CIMShooterMotor.setNeutralMode( NeutralMode.Brake);
+  }
+
+  // sets speed from -1 to 1
+  public void setSpeed(double speed) {
+    CIMShooterMotor.set(ControlMode.PercentOutput, speed);
+  }
+}
